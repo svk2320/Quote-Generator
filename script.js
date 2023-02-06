ab4a6a26ddc5b7c52bb47157dfb5dd991e4cf2aa
vasanth2401 <vasanthkumars2025@gmail.com>
2023-02-06 18:11:13 +0530

    adding api to script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 let apiQuotes = [];
 
+// Show New Quote
+function newQuote() {
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quotes From API
 async function getQuote() {
     const apiURL = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiURL);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+        newQuote();
     } catch (error) {
         // Catch Error Here
         alert(error);
